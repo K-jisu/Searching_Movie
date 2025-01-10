8120670f471ba990cdd2293ea341ae027d21b7b8
K-jisu <wltn7star@naver.com>
2025-01-10 10:53:48 +0900

    ✨ update : api 모듈화

--- src/api.js
+++ src/api.js
@@ -1,15 +1,34 @@
 /**API */
+import { key } from "./key.js";
+const option = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      key,
+  },
+};
+
 export async function getNowPlayingMovies() {
   const res = await fetch(
     "https://api.themoviedb.org/3/movie/now_playing?language=ko&page=1&region=KR",
-    {
-      method: "GET",
-      headers: {
-        accept: "application/json",
-        Authorization:
-          "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4NDJjZWYzY2E0ZmMyMzkzNWZmMjJlYmMyOTZkZjRkMCIsIm5iZiI6MTczNjI5ODc3MC45ODUsInN1YiI6IjY3N2RkMTEyODlmYzVkOTQ0MjRlNThlNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.onmKezQdK2pZe252BOkjXWX4QuaTvU-fa7cpLnROSe0",
-      },
-    }
+    option
+  );
+  return res.json();
+}
+
+export async function getMovies(movieId) {
+  const res = await fetch(
+    `https://api.themoviedb.org/3/movie/${movieId}?language=ko`,
+    option
+  );
+  return res.json();
+}
+
+export async function getSearch(movieSearch) {
+  const res = await fetch(
+    `https://api.themoviedb.org/3/search/movie??query=${movieSearch}include_adult=false&language=ko&page=1region=KR`,
+    option
   );
   return res.json();
 }
