1a8b34869bf605bf3ffcd0f23ece718ae859134a
K-jisu <wltn7star@naver.com>
2025-01-10 21:35:27 +0900

    ✨ update : popular api 추가, 구현

--- src/api.js
+++ src/api.js
@@ -4,31 +4,54 @@ const option = {
   method: "GET",
   headers: {
     accept: "application/json",
-    Authorization:
-      key,
+    Authorization: key,
   },
 };
 
 export async function getNowPlayingMovies() {
-  const res = await fetch(
-    "https://api.themoviedb.org/3/movie/now_playing?language=ko&page=1&region=KR",
-    option
-  );
-  return res.json();
+  try {
+    const res = await fetch(
+      `https://api.themoviedb.org/3/movie/now_playing?language=ko&page=1&region=KR`,
+      option
+    );
+    return res.json();
+  } catch {
+    console.log("getNowPlaying : ", error);
+  }
 }
 
 export async function getMovies(movieId) {
-  const res = await fetch(
-    `https://api.themoviedb.org/3/movie/${movieId}?language=ko`,
-    option
-  );
-  return res.json();
+  try {
+    const res = await fetch(
+      `https://api.themoviedb.org/3/movie/${movieId}?language=ko`,
+      option
+    );
+    return res.json();
+  } catch {
+    console.log("getMovie : ", error);
+  }
 }
 
 export async function getSearch(movieSearch) {
-  const res = await fetch(
-    `https://api.themoviedb.org/3/search/movie??query=${movieSearch}include_adult=false&language=ko&page=1region=KR`,
-    option
-  );
-  return res.json();
+  try {
+    const res = await fetch(
+      `https://api.themoviedb.org/3/search/movie?query=${movieSearch}&include_adult=false&language=ko&page=1&region=KR`,
+      option
+    );
+    return res.json();
+  } catch (error) {
+    console.log("getSearch : ", error);
+  }
+}
+
+export async function getPopular() {
+  try {
+    const res = await fetch(
+      `https://api.themoviedb.org/3/movie/popular?language=ko&page=1&region=KR`,
+      option
+    );
+    return res.json();
+  } catch (error) {
+    console.log("getPopular : ", error);
+  }
 }
--- src/main.js
+++ src/main.js
@@ -1,62 +1,26 @@
-import { getNowPlayingMovies } from "./api.js";
-// movies 데이터
-const movies = [];
+import { getSearch, getPopular } from "./api.js";
+import { template } from "./template.js";
 
 const input = document.getElementById("header__input");
+const section = document.getElementById("section");
 
-input.addEventListener("keyup", () => {
-  searchMovie();
+// search 기능
+input.addEventListener("keyup", async (e) => {
+  section.innerHTML = "";
+  const filteredMovie = await getSearch(e.target.value);
+  const value = filteredMovie.results;
+  template(value);
 });
 
+// popular 카드 보여주기
 const postMovie = async () => {
   try {
-    const data = await getNowPlayingMovies();
-    data.results.forEach((movie) => {
-      const movieData = {
-        id: movie.id,
-        title: movie.title,
-        detail: movie.overviw,
-        poster: movie.poster_path,
-        average: movie.vote_average,
-        date: movie.release_date,
-      };
-      movies.push(movieData);
-
-      const sectionCard = document.createElement("div");
-      sectionCard.classList.add("section__card");
-
-      const template = `
-      <div class="section__image">
-        <img src = "https://image.tmdb.org/t/p/w200/${movieData.poster}" alt = "poster"/>
-      </div>
-      <div class="section__info">
-        <h2 class="section__moviename">${movieData.title}</h2>
-        <p>평점 : ${movieData.average}</p>
-      </div>`;
-      sectionCard.innerHTML = template;
-      document.getElementById("section").appendChild(sectionCard);
-    });
+    const data = await getPopular();
+    const movielist = data.results;
+    template(movielist);
   } catch (error) {
     console.log(error);
   }
 };
 postMovie();
 
-const searchMovie = () => {
-  const search = input.value;
-  // section__card를 가져옴
-  const sectCard = document.querySelectorAll(".section__card");
-  // console.log(sectCard)
-  for (const sectName of sectCard) {
-    // section__card에서 section__moviename을 가져옴
-    const movieName = sectName.querySelector(".section__moviename").innerHTML.normalize("NFKD")
-    // section__moviename에 search값이 있으면 filtered 제거 없으면 filtered 추가.
-    // console.log(movieName)
-    if (movieName.includes(search.normalize("NFKD"))) {
-      sectName.classList.remove("filtered");
-      console.log("filtered")
-    } else {
-      sectName.classList.add("filtered");
-    }
-  }
-};
