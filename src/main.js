e551ff30c7ff0f2445c6008999cd3bfb99e75ebc
K-jisu <wltn7star@naver.com>
2025-01-14 15:56:00 +0900

    ✨ update : modal 세부사항 추가

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
-import { getSearch, getPopular } from "./api.js";
-import { template } from "./template.js";
+import { getSearch, getPopular, getDetails } from "./api.js";
+import { modalTemplate, template } from "./template.js";
 
 const input = document.getElementById("header__input");
 const section = document.getElementById("section");
-
+const modalID = document.getElementById("modal");
+const sectionCard = document.getElementsByClassName("section__card");
 // search 기능
 input.addEventListener("keyup", async (e) => {
   section.innerHTML = "";
@@ -11,6 +12,7 @@ input.addEventListener("keyup", async (e) => {
   const value = filteredMovie.results;
   template(value);
 });
+
 
 // popular 카드 보여주기
 const postMovie = async () => {
@@ -24,12 +26,27 @@ const postMovie = async () => {
 };
 postMovie();
 
+
 // 모달 구현해야지
-const modalID = document.getElementById("modal");
-section.addEventListener("click", (e)=>{
-  // modalID.classList.remove("dpn")
-  console.log(e.target)
-})
-// 모달에 데이터 넣어야지
+section.addEventListener("click", async (e) => {
+  const secTarget = e.target.closest(".section__card")
+  const secTargetId = +secTarget.getAttribute("mid")
+  if (secTarget) {
+    const getData = await getDetails(secTargetId)
+    modalID.innerHTML = modalTemplate(getData)
+    document.body.style.overflow = "hidden"
+    console.log(secTargetId)
+    console.log(getData)
+    modalID.classList.remove("dpn");
+    
+  }
+});
+modalID.addEventListener("click", (e) => {
+  if (!e.target.closest(".modal__content")) {
+    modalID.classList.add("dpn");
+  }
+});
+
+
 // 무한 스크롤 만들어야지
 // 할게 많구만
--- src/nowplaying.js
+++ src/nowplaying.js
@@ -1,7 +1,8 @@
-import { getNowPlayingMovies } from "./api.js";
-import { template } from "./template.js";
+import { getNowPlayingMovies, getDetails } from "./api.js";
+import { template, modalTemplate } from "./template.js";
 
 const input = document.getElementById("header__input");
+const section = document.getElementById("section");
 
 const postMovie = async () => {
   try {
@@ -27,8 +28,8 @@ const searchMovie = () => {
   for (const sectName of sectCard) {
     // section__card에서 section__moviename을 가져옴
     const movieName = sectName
-      .querySelector(".section__moviename")
-      .innerHTML.normalize("NFKD");
+    .querySelector(".section__moviename")
+    .innerHTML.normalize("NFKD");
     // section__moviename에 search값이 있으면 filtered 제거 없으면 filtered 추가.
     // console.log(movieName)
     if (movieName.includes(search.normalize("NFKD"))) {
@@ -39,3 +40,25 @@ const searchMovie = () => {
     }
   }
 };
+
+const modalID = document.getElementById("modal")
+console.log(modalID)
+
+// 모달 구현해야지
+section.addEventListener("click", async (e) => {
+  const secTarget = e.target.closest(".section__card");
+  if (secTarget) {
+    const secTargetId = +secTarget.getAttribute("mid");
+    const getData = await getDetails(secTargetId);
+    modalID.innerHTML = modalTemplate(getData);
+    document.body.style.overflow = "hidden";
+    console.log(secTargetId);
+    console.log(getData);
+    modalID.classList.remove("dpn");
+  }
+});
+modalID.addEventListener("click", (e) => {
+  if (!e.target.closest(".modal__content")) {
+    modalID.classList.add("dpn");
+  }
+});
