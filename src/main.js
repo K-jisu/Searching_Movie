c48fc6da72d781e22d0d76c44d9b42809f9e4711
K-jisu <wltn7star@naver.com>
2025-01-15 20:51:25 +0900

    ✨ update : 북마크 , 디바운싱 추가

--- src/main.js
+++ src/main.js
@@ -4,13 +4,22 @@ import { modalTemplate, template } from "./template.js";
 const input = document.getElementById("header__input");
 const section = document.getElementById("section");
 const modalID = document.getElementById("modal");
-const sectionCard = document.getElementsByClassName("section__card");
+const goBookMark = document.getElementById("gobookmark");
+
 // search 기능
-input.addEventListener("keyup", async (e) => {
-  section.innerHTML = "";
-  const filteredMovie = await getSearch(e.target.value);
-  const value = filteredMovie.results;
-  template(value);
+// debounce 추가
+let debounceTimer;
+input.addEventListener("input", (e) => {
+  clearTimeout(debounceTimer);
+  debounceTimer = setTimeout(async () => {
+    section.innerHTML = "";
+    const filteredMovie = await getSearch(e.target.value);
+    const value = filteredMovie.results;
+    template(value);
+    if (e.target.value.length === 0) {
+      postMovie();
+    }
+  }, 500);
 });
 
 // popular 카드 보여주기
@@ -25,24 +34,50 @@ const postMovie = async () => {
 };
 postMovie();
 
-// 모달 구현해야지
+// 모달 UI
 section.addEventListener("click", async (e) => {
-  const secTarget = e.target.closest(".section__card");
-  const secTargetId = +secTarget.getAttribute("mid");
-  if (secTarget) {
-    const getData = await getDetails(secTargetId);
+  const sectionTarget = e.target.closest(".section__card");
+  if (!sectionTarget) {
+    return;
+  } else if (sectionTarget) {
+    const sectionTargetId = +sectionTarget.getAttribute("mid");
+    const getData = await getDetails(sectionTargetId);
     modalID.innerHTML = modalTemplate(getData);
     document.body.style.overflow = "hidden";
-    console.log(secTargetId);
-    console.log(getData);
-    modalID.classList.remove("dpn");
+    modalID.classList.remove("hidden");
   }
 });
+
+// 북마크 추가
+const idArr = new Set();
 modalID.addEventListener("click", (e) => {
+  const bookMarkBtn = e.target.closest("#bookmark");
+  // 북마크 버튼 누를때
+  if (bookMarkBtn) {
+    const modalConatiner = document.getElementById("modal__container");
+    const movieId = modalConatiner.getAttribute("mid");
+    idArr.add(+movieId);
+    localStorage.setItem("id", JSON.stringify([...idArr]));
+  }
+  // 모달 창 닫기
   if (!e.target.closest(".modal__content")) {
-    modalID.classList.add("dpn");
+    modalID.classList.add("hidden");
     document.body.style.overflow = "auto";
   }
+});
+
+// 북마크 확인하기
+goBookMark.addEventListener("click", async () => {
+  section.innerHTML = "";
+  const LocalData = localStorage.getItem("id");
+  const idList = [...JSON.parse(LocalData)];
+  const dataList = await Promise.all(
+    idList.map(async (movieId) => {
+      const res = await getDetails(movieId);
+      return res;
+    })
+  );
+  template(dataList);
 });
 
 // 무한 스크롤 만들어야지
