0a9bccc1a4ee1630ce9e4c6f857f97953582054d
K-jisu <wltn7star@naver.com>
2025-01-15 20:49:58 +0900

    ✨ update : 데이터 구조분해 할당

--- src/template.js
+++ src/template.js
@@ -1,25 +1,19 @@
 export const template = (movielist) => {
   movielist.forEach((movie) => {
-    const movieData = {
-      id: movie.id,
-      title: movie.title,
-      detail: movie.overview,
-      poster: movie.poster_path,
-      average: movie.vote_average,
-      date: movie.release_date,
-    };
+    // 구조분해할당 알아보기
+    const { id, title, poster_path, vote_average } = movie;
 
     const sectionContiner = document.createElement("div");
     sectionContiner.classList.add("section__container");
 
     const template = `
-    <div class = "section__card" mid=${movieData.id}>
+    <div class = "section__card" mid=${id}>
       <div class="section__image">
-      <img src = "https://image.tmdb.org/t/p/w200/${movieData.poster}" alt = "poster"/>
+      <img src = "https://image.tmdb.org/t/p/w200/${poster_path}" alt = "poster"/>
       </div>
       <div class="section__info">
-      <h2 class="section__moviename">${movieData.title}</h2>
-      <p>평점 : ${movieData.average}</p>
+      <h2 class="section__moviename">${title}</h2>
+      <p>평점 : ${vote_average}</p>
       </div>
       </div>`;
     sectionContiner.innerHTML = template;
@@ -27,26 +21,20 @@ export const template = (movielist) => {
   });
 };
 
-export const modalTemplate = (movieId) => {
-  const movie = {
-    title: movieId.title,
-    poster: movieId.poster_path,
-    detail: movieId.overview,
-    average: movieId.vote_average,
-    date: movieId.release_date,
-  };
+export const modalTemplate = (movie) => {
+  const { id, title, overview, poster_path, vote_average, release_date } = movie;
   return `
-        <div class="modal__content">
+        <div id="modal__container" class="modal__content" mid=${id}>
           <div class="modal__img">
-            <img src = "https://image.tmdb.org/t/p/w400/${movie.poster}" alt = "poster"/>
+            <img src = "https://image.tmdb.org/t/p/w400/${poster_path}" alt = "poster"/>
           </div>
           <div class="modal__info">
-            <h2 class="modal__title">${movie.title}</h2>
-            <p class="modal__desc">${movie.detail}</p>
-            <h3 class="modal__date">개봉일 : ${movie.date}</h3>
-            <h3 class="modal__rating">평점 : ${movie.average}</h3>
+            <h2 class="modal__title">${title}</h2>
+            <p class="modal__desc">${overview}</p>
+            <h3 class="modal__date">개봉일 : ${release_date}</h3>
+            <h3 class="modal__rating">평점 : ${vote_average}</h3>
           </div>
-          <button id="addBookmark" class="modal__bookmark">북마크 추가</button>
+          <button id="bookmark" class="modal__bookmark">북마크 추가</button>
         </div>
 `;
 };
