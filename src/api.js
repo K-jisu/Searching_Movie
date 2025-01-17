6e91bee18976d301311fc9e4e74c178486de4235
K-jisu <wltn7star@naver.com>
2025-01-17 10:20:54 +0900

    🐛 key값 수정

--- src/api.js
+++ src/api.js
@@ -1,10 +1,10 @@
 /**API */
-// import { key } from "./key.js";
+import { key } from "./key.js";
 const option = {
   method: "GET",
   headers: {
     accept: "application/json",
-    Authorization: "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4NDJjZWYzY2E0ZmMyMzkzNWZmMjJlYmMyOTZkZjRkMCIsIm5iZiI6MTczNjI5ODc3MC45ODUsInN1YiI6IjY3N2RkMTEyODlmYzVkOTQ0MjRlNThlNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.onmKezQdK2pZe252BOkjXWX4QuaTvU-fa7cpLnROSe0"
+    Authorization: key,
   },
 };
 
